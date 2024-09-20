aff1e364835c6d300a1b29f89bd28aad16878697
maidan-mykhailo <mykhailo.maidan@yalantis.net>
2024-09-20 15:10:00 +0200

    Delete prisma project

--- src/common/prisma/prisma.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
-
-@Injectable()
-export class PrismaService extends PrismaClient {} 
--- src/modules/user/interfaces/interfaces.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "@prisma/client";
-import { UserDTO } from "../dto/create-user.dto";
-
-export interface IUserRepository{
-    createUser(user: UserDTO);
-}
